a8760bd6307b25de19ff71895314c276e99e12c7
discdiver <jeffmshale@gmail.com>
2016-02-01 17:27:35 -0500

    adds todo file, gets all tests passing

---
+++ todos.js
@@ -0,0 +1,18 @@
+/**
+Todo file
+
+write tests for comments
+add slugs
+change formatting for comments and generally
+make sure user account privlidges work better
+add support markdown
+add image input support
+
+add comment moderation
+
+add mailchimp form
+
+then integrate home into static kathleenh page maybe - or serve kathleenhpage somewhere else - can I just have it all made static and then just serve it somewhere for like almost no cost?
+
+
+*/
